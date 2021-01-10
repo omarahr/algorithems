fa0b4f3b52dfe24376452433ced7a829952b5157
or <omar.solve@gmail.com>
2021-01-10 22:12:37 +0400

    leet code fb dp problem

---
+++ src/com/company/algoexpert/binarytree/nextsucessor/Solution.java
@@ -0,0 +1,38 @@
+package com.company.algoexpert.binarytree.nextsucessor;
+
+public class Solution {
+
+
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left = null;
+        public BinaryTree right = null;
+        public BinaryTree parent = null;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+
+    public BinaryTree findSuccessor(BinaryTree tree, BinaryTree node) {
+        if (node.right != null)
+            return getLeftMostChild(node.right);
+
+        return getRightMostParent(node);
+    }
+
+    private BinaryTree getRightMostParent(BinaryTree node) {
+        BinaryTree current = node;
+        while (current.parent != null && current.parent.right == current)
+            current = current.parent;
+        return current;
+    }
+
+
+    private BinaryTree getLeftMostChild(BinaryTree node) {
+        BinaryTree current = node;
+        while (current.left != null)
+            current = current.left;
+        return current;
+    }
+}
---
+++ src/com/company/leetcode/facebook/dynamicprogramming/longestpalindrom/Solution.java
@@ -0,0 +1,47 @@
+package com.company.leetcode.facebook.dynamicprogramming.longestpalindrom;
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+
+        int start = 0, end = 0;
+        char[] str = s.toCharArray();
+
+        for (int i = 0; i < s.length(); i++) {
+            int evenLen = expandFromTheMiddle(str, i, i + 1);
+            int oddLen = expandFromTheMiddle(str, i, i);
+
+            int len = Math.max(evenLen, oddLen);
+            if (len > end - start) {
+                start = i - ((len - 1) /2);
+                end = i + (len/2);
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expandFromTheMiddle(char[] str, int start, int end) {
+        if (start > end || str.length == 0)
+            return 0;
+
+        while (start >= 0 && end < str.length && str[start] == str[end]) {
+            start--;
+            end++;
+        }
+
+        return end - start - 1;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("babad"));
+        System.out.println(solution.longestPalindrome("cbbd"));
+    }
+}
