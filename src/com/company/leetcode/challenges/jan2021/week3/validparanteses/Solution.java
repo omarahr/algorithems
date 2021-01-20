924cc1d16f6583d38e7b791b44259ec7566f2aa9
or <omar.solve@gmail.com>
2021-01-20 18:20:28 +0400

    jan 2021 leet code challenge third week

---
+++ src/com/company/leetcode/challenges/jan2021/week3/validparanteses/Solution.java
@@ -0,0 +1,45 @@
+package com.company.leetcode.challenges.jan2021.week3.validparanteses;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if (isOpening(x)) {
+                stack.add(x);
+            } else if (stack.isEmpty()){
+                return false;
+            } else {
+                boolean match = matches(stack.pop(), x);
+                if (!match)
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean matches(char open, char close) {
+        return (close == ']' && open == '[')
+                || (close == '}' && open == '{')
+                || (close == ')' && open == '(');
+    }
+
+    private boolean isOpening(char x) {
+        return x == '(' || x == '{' || x == '[';
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.isValid("()"));
+        System.out.println(solution.isValid("()[]{}"));
+        System.out.println(solution.isValid("(]"));
+        System.out.println(solution.isValid("([)]"));
+        System.out.println(solution.isValid("{[]}"));
+    }
+}
